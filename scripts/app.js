1cee93af1ccc3300c112c7c12393792f2b06af91
sssnakes <finnan.barry@gmail.com>
2017-05-17 21:06:56 +0200

    first draft

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $('#menu-butt').click(function(){
+    $('.menu').css('display', 'block');
+    $('.purple').css('display', 'none');
+  });
+
+  $('#menu-close').click(function(){
+    $('.menu').css('display', 'none');
+  });
+
+  // $('.menu-select').click(function(){
+  //   var t = $(this).data('data');
+  //
+  //   $('.')
+  // });
+
+});
