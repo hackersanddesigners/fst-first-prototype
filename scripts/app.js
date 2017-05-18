ef7016ab8c538dc22015165cac4d3b0b35f27a1e
sssnakes <finnan.barry@gmail.com>
2017-05-18 15:45:53 +0200

    major pieces in place, minor bug fixes needed

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,40 @@
 $(document).ready(function(){
 
-  $('#menu-butt').click(function(){
+  $('.menu-butt').click(function(){
     $('.menu').css('display', 'block');
     $('.purple').css('display', 'none');
+    // STOP SCROLLING
+    $('body').css('position', 'fixed');
   });
 
   $('#menu-close').click(function(){
     $('.menu').css('display', 'none');
+    $('body').css('position', 'static');
   });
 
-  // $('.menu-select').click(function(){
-  //   var t = $(this).data('data');
-  //
-  //   $('.')
-  // });
+  $('.menu-select').click(function(){
+    var att = $(this).attr('value'),
+        typ = '#info-' + att;
+
+    if ( att == 0 ){
+      $('.menu').css('display', 'none');
+      $('body').css('position', 'static');
+      console.log('yep');
+    } else {
+      $('.menu').css('display', 'none');
+      $(typ).css('display', 'block');
+
+      console.log('nope');
+    }
+
+  });
+
+  // SIDEBAR
+
+  $('.sidebar-close').click(function(){
+    $('.sidebar-left').animate({
+      left: -580
+    }, 200);
+  });
 
 });
