93d447c2fb3d41c1d988dbd3b857f1efe74a5476
sssnakes <finnan.barry@gmail.com>
2017-05-20 17:06:41 +0200

    a_language_code_t static search working

--- scripts/app.js
+++ scripts/app.js
@@ -3,26 +3,45 @@ var staticTexts = {
   'publishing_country_t': "Lorem ipsum",
 }
 
-
-
-
+// var facetTopics = {
+//   'Language of Publication': 'language_code_t',
+// }
 
 
 var doSearch = function(facet, str) {
   console.log("Do the search: " + str);
-
-  // language_code_t
-  // publishing_country_t
-  //
 
   $.get('http://localhost:8888/solr?rows=0&facet=true&facet.field=' + facet + '&q=' + str, function(res) {
     var data = JSON.parse(res);
     var totalCount = data.response.numFound;
     console.log("total count: " + totalCount);
     var facets = data.facet_counts.facet_fields[facet];
-    for(var i = 0; i < facets.length; i++) {  
+
+    for(var i = 0; i < facets.length; i++) {
       console.log(facets[i]);
       // Show search results....
+
+    }
+
+
+    if ( totalCount == 0 ){
+
+      $('.results-container').html('<div class="results-title serif"><p>Results:</p></div>');
+      $('.results-container').append('</br><div class="results-records serif"><p>There were no records found for: <span class="sans results-subject">"' + str + '"</span>');
+
+    } else{
+      var percentage1 = Math.floor((facets[1] / totalCount) * 100);
+      var percentage2 = Math.floor((facets[3] / totalCount) * 100);
+      var percentage3 = Math.floor((facets[5] / totalCount) * 100);
+      var percentage4 = Math.floor((facets[7] / totalCount) * 100);
+
+      $('.results-container').html('<div class="results-title serif"><p>Results:</p></div>');
+
+      // X Records for Y
+      $('.results-container').append('</br><div class="results-records serif"><p><span class="sans results-answer">'+ totalCount +'</span> records found for: <span class="sans results-subject">"' + str + '"</span></br>');
+
+      // Of which %
+      $('.results-container').append('</br><div class="results-answer-box serif"><p>Of which:</p><p>Search question related to <span class="result-topic">language</span></p></br><div class="sans"><p>' + percentage1 + '% in ' + facets[0] + '</br>' + percentage2 + '% in ' + facets[2]+ '</br>' + percentage3 + '% in ' + facets[4] + '</br>' + percentage4 + '% in ' + facets[6]);
 
     }
 
@@ -69,9 +88,12 @@ $(document).ready(function(){
     }, 200);
   });
 
+  // SEARCH
+
   $('input[type=submit]').click(function(e) {
     e.preventDefault();
-    doSearch('language_code_t', $('input[name=searchf]').val());
+    doSearch('a_language_code_t', $('input[name=searchf]').val());
+
   });
 
 });
