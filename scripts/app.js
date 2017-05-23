d6aa36485183f704da711b1219a9f32348603e47
JBG <jbg@hackersanddesigners.nl>
2017-05-23 09:36:04 +0000

    tweaks by jbg/finn during meeting

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,10 @@ var staticTexts = {
 var facetArray = [
   ['Language',                'language_code_t'],
   ['Birth Name of Author',    'birth_d_personal_name_t'],
-  ['Geographic Area',         'a_geographic_area_code_t']
+  ['Geographic Area',         'a_geographic_area_code_t'],
+  ['Translated item (Item is or includes a translation)', 'h_language_code_t'],
+  ['Place of publication', 'a_geographic_area_code_t'],
+  ['Multiple languages', 'a_language_code_t', 'count_a_language_code_i:[2 TO *]']
 ];
 
 
@@ -29,8 +32,9 @@ var mainSearch = function(str) {
 
     var facetTopic = facetArray[i][0];
     var facetCode  = facetArray[i][1];
+    var facetPred  = facetArray[i][2];
 
-    facetSearch(facetCode, facetTopic, str);
+    facetSearch(facetCode, facetTopic, facetPred, str);
 
   }
 
@@ -39,9 +43,15 @@ var mainSearch = function(str) {
 };
 
 
-var facetSearch = function(code, top, str) {
+var facetSearch = function(code, top, pred, str) {
+  var url = 'http://' + window.location.host + '/solr?rows=0&facet=true&facet.field=' + code + '&q=' + str;
 
-  $.get('http://localhost:8888/solr?rows=0&facet=true&facet.field=' + code + '&q=' + str, function(res) {
+  if(pred) {
+   url += ' AND ' + pred;
+  }
+  console.log(url);
+
+  $.get(url, function(res) {
 
     var data = JSON.parse(res);
     var totalCount = data.response.numFound;
@@ -58,21 +68,22 @@ var facetSearch = function(code, top, str) {
     var percentage3 = Math.floor((facets[5] / totalCount) * 100);
     var percentage4 = Math.floor((facets[7] / totalCount) * 100);
 
-    $('.results-answer-box').append('</br></br><p>Search question related to <span class="result-topic" onclick="topicDisplay(this)" data-topicval="' + code + '" data-topic="' + top + '">' + top + '</span></p></br><div class="sans"><p><span class="percentage">' + percentage1 + '%</span> in <span class="upper">' + facets[0] + '</span></br><span class="percentage">' + percentage2 + '%</span> in <span class="upper">' + facets[2]+ '</span></br><span class="percentage">' + percentage3 + '%</span> in <span class="upper">' + facets[4] + '</span></br><span class="percentage">' + percentage4 + '%</span> in <span class="upper">' + facets[6] + '</span></div>');
+    $('.results-answer-box').append('</br></br><p>Search question related to <span id=' + code + ' class="result-topic" onclick="topicDisplay(this)" data-topicval="' + code + '" data-topic="' + top + '">' + top + '</span></p></br><div class="sans"><p><span class="percentage">' + percentage1 + '%</span> in <span class="upper">' + facets[0] + '</span></br><span class="percentage">' + percentage2 + '%</span> in <span class="upper">' + facets[2]+ '</span></br><span class="percentage">' + percentage3 + '%</span> in <span class="upper">' + facets[4] + '</span></br><span class="percentage">' + percentage4 + '%</span> in <span class="upper">' + facets[6] + '</span></div>');
 
+  }).fail(function(e) {
+    console.log("FAIL?!?!?!? " + JSON.stringify(e));
   });
 
 };
 
 
 var topicDisplay = function(e) {
-
   var a = e.getAttribute("data-topicval");
   var g = e.getAttribute("data-topic");
-  var r = staticTexts.a;
+  var r = staticTexts[a];
   console.log(r);
 
-  $('.sidebar-text-container').html('<div class="sidebar-text serif"><p>Search question related to <span>country of publishing</span>:</p></div>');
+  $('.sidebar-text-container').html('<div class="sidebar-text serif"><p>Search question related to <span>country of publishing</span>' + r + '</p></div>');
 
   $('.sidebar-left').animate({
     left: 0
@@ -83,6 +94,8 @@ var topicDisplay = function(e) {
 
 
 $(document).ready(function(){
+
+  $('iframe').attr('src', 'https://' + window.location.host + '/ep');
 
   $('.result-topic').click(function(){
     console.log('worked');
