676afc19317ea4e8f48e8796b00b3f270011cf60
sssnakes <finnan.barry@gmail.com>
2017-05-24 23:56:01 +0200

    hard times with $.when()

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,33 @@
 // THINGS TO DO:
-// - Move totalCount out from second function into new
-// - Attach staticTexts object to html() function under topicDisplay()
+// - Fix floor problem with percentage Math
+// - Pull totalCount out using Promise
+// - Add all available texts
 
 
 var staticTexts = {
-  'language_code_t': "Lorem ipsum....",
-  'publishing_country_t': "Lorem ipsum",
+  'Language of Publication':            'The filter Language of publication refers to the predominant language of a publication*. It is an interesting filter to consider in the light of our question: Why are the authors of the books I read so white, so male, so Eurocentric?, because it shows how difficult is is to make conclusions about race and nationality of the author on the basis of the language of an item. Despite those shortcomings, this filter clearly reflects that English remains the dominant language of global scholarly publishing, with 81,52% of the items of this data set being published in English. This highlights a linguistic monoculture, that scholars such as Gloria E. Anzaldúa have long tried to criticize and attempted to intervene in. Additionally, the fact that English, Dutch, French, Italian and German are the five most frequent languages of publication is – despite of the occurrence of some of these languages in different geographical contexts – still very telling for how our Western knowledge economy functions.*This filter is an interpretation of the MARC 21 tag 008 - Fixed-Length Data Elements character positions 35-37 - Language.',
+  'Place of publication':               'This filter refers to the country, in which a work has been published.* It is an interesting filter, as it shows the geographical locations, around which current global scholarly publishing centres. The most frequently named locations in this context are the United States, with New York, and the United Kingdom, with London and Cambridge.',
 }
 
 var facetArray = [
-  ['Language',                'language_code_t'],
-  ['Birth Name of Author',    'birth_d_personal_name_t'],
-  ['Geographic Area',         'a_geographic_area_code_t'],
-  ['Translated item (Item is or includes a translation)', 'h_language_code_t'],
-  ['Place of publication', 'a_geographic_area_code_t'],
-  ['Multiple languages', 'a_language_code_t', 'count_a_language_code_i:[2 TO *]']
+  ['Language of publication',                               'language_code_t'],
+  ['Language of the item (Predominant language)',           'a_language_code_t'],
+  ['Multiple languages',                                    'a_language_code_t', 'count_a_language_code_i:[2 TO *]'],
+  ['Translated item (Item is or includes a translation)',   'h_language_code_t', 'h_language_code_t:*'],
+  ['Original language (of translated item)',                'a_language_code_t'],
+  ['Place of publication',                                  'a_geographic_area_code_t', 'a_geographic_area_code_t:*'],
+  ['Multiple languages',                                    'a_language_code_t', 'count_a_language_code_i:[2 TO *]'],
+  ['Country of Publishing/Producing entity',                'a_publishing_country_t'],
+  ['Multiple places',                                       'a_publishing_country_t', 'count_a_publishing_country_i:[2 TO *]'],
+  ['Publisher',                                             'b_imprint_s'],
+  ['Manufacturer',                                          'f_imprint_s'],
+  ['Time period of Publication',                            'c_imprint_s'],
+  ['Date of publication',                                   'copyright_s'],
+  ['(Personal) Information about the author',               'a_creator_characteristics_t'],
+  ['Profession of the author',                              'e_personal_name_t'],
+  ['Titles held by the author',                             'c_personal_name_t'],
+  ['Authors dead/alive',                                    'death_d_personal_name_t', 'birth_d_personal_name_t'],
+  ['Gender',                                                'gender_s']
 ];
 
 
@@ -51,27 +64,29 @@ var facetSearch = function(code, top, pred, str) {
   }
   console.log(url);
 
-  $.get(url, function(res) {
+  // $.when(
 
-    var data = JSON.parse(res);
-    var totalCount = data.response.numFound;
-    console.log("total count: " + totalCount);
-    var facets = data.facet_counts.facet_fields[ code ];
+    $.get(url, function(res) {
 
-    // SHOW RESULTS
-    // for(var i = 0; i < facets.length; i++) {
-    //   console.log(facets[i]);
-    // }
+      var data = JSON.parse(res);
+      var totalCount = data.response.numFound;
+      console.log("total count: " + totalCount);
+      var facets = data.facet_counts.facet_fields[ code ];
 
-    var percentage1 = Math.floor((facets[1] / totalCount) * 100);
-    var percentage2 = Math.floor((facets[3] / totalCount) * 100);
-    var percentage3 = Math.floor((facets[5] / totalCount) * 100);
-    var percentage4 = Math.floor((facets[7] / totalCount) * 100);
+  //   })
+  //
+  // ).then(function(){
+  //
+  //   var facets = data.facet_counts.facet_fields[ code ];
+  //   console.log( facets );
+    var percentage1 = (facets[1] / 100) * 100;
+    var percentage2 = (facets[3] / 100) * 100;
+    var percentage3 = (facets[5] / 100) * 100;
+    var percentage4 = (facets[7] / 100) * 100;
+    var percentageA = Math.round(percentage1 * 100) / 100;
 
-    $('.results-answer-box').append('</br></br><p>Search question related to <span id=' + code + ' class="result-topic" onclick="topicDisplay(this)" data-topicval="' + code + '" data-topic="' + top + '">' + top + '</span></p></br><div class="sans"><p><span class="percentage">' + percentage1 + '%</span> in <span class="upper">' + facets[0] + '</span></br><span class="percentage">' + percentage2 + '%</span> in <span class="upper">' + facets[2]+ '</span></br><span class="percentage">' + percentage3 + '%</span> in <span class="upper">' + facets[4] + '</span></br><span class="percentage">' + percentage4 + '%</span> in <span class="upper">' + facets[6] + '</span></div>');
+    $('.results-answer-box').append('</br></br><p>Search question related to <span id=' + code + ' class="result-topic" onclick="topicDisplay(this)" data-topicval="' + top + '" data-topic="' + top + '">' + top + '</span></p></br><div class="sans"><p><span class="percentage">' + percentageA + '%</span> in <span class="upper">' + facets[0] + '</span></br><span class="percentage">' + percentage2 + '%</span> in <span class="upper">' + facets[2]+ '</span></br><span class="percentage">' + percentage3 + '%</span> in <span class="upper">' + facets[4] + '</span></br><span class="percentage">' + percentage4 + '%</span> in <span class="upper">' + facets[6] + '</span></div>');
 
-  }).fail(function(e) {
-    console.log("FAIL?!?!?!? " + JSON.stringify(e));
   });
 
 };
@@ -83,7 +98,7 @@ var topicDisplay = function(e) {
   var r = staticTexts[a];
   console.log(r);
 
-  $('.sidebar-text-container').html('<div class="sidebar-text serif"><p>Search question related to <span>country of publishing</span>' + r + '</p></div>');
+  $('.sidebar-text-container').html('<div class="sidebar-text serif"><p>Search question related to <span>' + g + '</span>:</p></br><p>' + r + '</p></div>');
 
   $('.sidebar-left').animate({
     left: 0
@@ -132,9 +147,23 @@ $(document).ready(function(){
 
   // SIDEBAR
   $('.sidebar-close').click(function(){
-    $('.sidebar-left').animate({
-      left: -580
-    }, 200);
+    var mobCheck = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if ( mobCheck ){
+
+      var w = $(window).width() - 20;
+      $('.sidebar-left').animate({
+        left: w
+      }, 200);
+    } else {
+      $('.sidebar-left').animate({
+        left: -580
+      }, 200);
+    }
+  });
+
+  $( window ).resize( function() {
+    $('.sidebar-left').css('left', 'calc(-90% - 20px);');
+    console.log('test');
   });
 
   // SEARCH
